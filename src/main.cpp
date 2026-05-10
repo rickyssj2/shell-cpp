7ec30351eeea5d44ad047cb046b3b6b6f00617d1
Aryan Pandey <aryanpandey048@gmail.com>
2026-05-10 15:15:03 -0700

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 #include <string>
 
+std::string strip (const std::string& str) {
+  size_t first = str.find_first_not_of(" \t\n\r");
+  if (first == std::string::npos) return "";
+  size_t last = str.find_last_not_of(" \t\n\r");
+  return s.substr(first, last - first + 1);
+}
+
+std::vector<std::string> strip (const std::string& str, char delim = ' ') {
+  std::vector<std::string> tokens;
+  std::stringstream ss(str);
+  std::string token;
+  while (std::getline(ss, token, delim)) 
+    tokens.emplace_back(token);
+  return tokens;
+}
+
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
@@ -10,11 +26,19 @@ int main() {
   int flag = 1;
   while (flag) {
     std::cout << "$ ";
-    std::string cmd;
-    std::cin >> cmd;
+    std::string input;
+    std::cin >> input;
+    input = strip(input);
+    std::vector<std::string> tokens = split(input);
+    std::string cmd = tokens[0];
+
     if (cmd == "exit") {
       flag = 0;
-      break;
+    } else if (cmd == "echo") {
+      for (int i = 0; i < tokens.size(); i++) {
+        std::cout << tokens[i] << " ";
+      }
+      std::cout << std::endl;
     } else {
       std::cout << cmd << ": command not found" << std::endl;
     }
